df5e28ed902a8e5c692d3da3ba3cfdd67b4e9b05
Javier Sanchez <javsan77@gmail.com>
2025-12-21 22:52:19 -0500

    Creation of Service Layer

---
+++ app/services/user_service.py
@@ -0,0 +1,26 @@
+from typing import List
+from fastapi import HTTPException, status
+
+from app.schemas.user_schema import UserCreate
+from app.repositories.user_repository import UserRepository
+
+class UserService:
+    def __init__(self):
+        self.repository = UserRepository()
+
+        def create_user(self, user: UserCreate) -> dict:
+            return self.repository.create(user)
+        
+        def get_all_users(self) -> List[dict]:
+            return self.repository.find_all()
+        
+        def get_user_by_id(self, user_id: int) -> dict:
+            user = self.repository.find_by_id(user_id)
+            if not user:
+                raise HTTPException(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    detail="User not found"
+                )
+            return user
+        
+        
