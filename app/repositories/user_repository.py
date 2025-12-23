f4aff1ebbc59ba685210fd8599c4da353db24e96
Javier Sanchez <javsan77@gmail.com>
2025-12-23 09:50:18 -0500

    We replaced code of simulation for database code

--- app/repositories/user_repository.py
+++ app/repositories/user_repository.py
@@ -1,4 +1,6 @@
 from typing import List, Optional
+from sqlalchemy import text
+from app.config.database import get_connection
 from app.schemas.user_schema import UserCreate
 
 class UserRepository:
@@ -6,21 +8,42 @@ class UserRepository:
         self._users: List[dict] = []
         self._id_counter: int = 1
 
-    def create(self, user: UserCreate)->dict:
-        new_user = {
-            "id": self._id_counter,
-            "name": user.name,
-            "email": user.email
-        }
-        self._users.append(new_user)
-        self._id_counter += 1
-        return new_user
+    def create(self, user: UserCreate)->int:
+        sql = text("""
+            EXEC dbo.usp_user_create
+                @Name = :name,
+                @Email = :email
+            """)
+
+        with get_connection() as conn:
+            result = conn.execute(sql, {
+                "name": user.name,
+                "email": user.email
+            })
+            new_id = result.scalar()
+            conn.commit()        
+
+        return new_id
     
     def find_all(self) -> List[dict]:
-        return self._users
+        sql = text("EXEC dbo.usp_user_list")
+
+        with get_connection() as conn:
+            result = conn.execute(sql)
+            rows = result.mappings.all()
+
+        return rows
     
     def find_by_id(self, user_id:int)->Optional[dict]:
-        for user in self._users:
-            if user["id"] == user_id:
-                return user
-            return None
+        sql = text("""
+                EXEC dbo.usp_user_get_by_id
+                @Id  = :id
+            """)
+
+        with get_connection() as conn:
+            result = conn.execute(sql, {"id":user_id})
+            row = result.mappings().first()
+        
+        return dict(row) if row else None
+
+
