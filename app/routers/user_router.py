4646c56f72a6396a8f96726f552a97a96b0f2cea
Javier Sanchez <javsan77@gmail.com>
2025-12-24 02:24:13 -0500

    Bearer works Ok

--- app/routers/user_router.py
+++ app/routers/user_router.py
@@ -4,6 +4,9 @@ from fastapi import APIRouter
 from app.schemas.user_schema import UserCreate, UserResponse
 from app.services.user_service import UserService
 
+from fastapi import Depends
+from app.dependencies.auth_dependency import get_current_user
+
 router = APIRouter()
 service = UserService()
 
@@ -12,7 +15,7 @@ def create_user(user:UserCreate):
     return service.create_user(user)
 
 @router.get("/", response_model=List[UserResponse])
-def get_users():
+def get_users(current_user = Depends(get_current_user)):
     return service.get_all_users()
 
 @router.get("/{user_id}", response_model=UserResponse)
